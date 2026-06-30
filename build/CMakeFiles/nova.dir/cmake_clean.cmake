2835e14a27ac35aed5c612f4221c6076836d5810
yahya-nova <yahyagoodarzi.10588@gmail.com>
2026-06-30 15:21:55 +0000

    add initial CMake build and Hello nova executable

---
+++ build/CMakeFiles/nova.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nova.dir/src/main.cpp.o"
+  "CMakeFiles/nova.dir/src/main.cpp.o.d"
+  "nova"
+  "nova.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nova.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
